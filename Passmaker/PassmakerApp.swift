1ad22052caf0a5b6e4d35cf8f2eeb63a48d4d288
Chris Williams <chriscancodethat@gmail.com>
2023-09-02 11:45:53 -0400

    Initial Commit

---
+++ Passmaker/PassmakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassmakerApp.swift
+//  Passmaker
+//
+//  Created by Chris Williams on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct PassmakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
